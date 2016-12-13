fafdc001e22d70d75a83f4f7b3993481060a56ef
chiar <chiar@DESKTOP-O7NRNIQ>
2016-12-13 22:44:53 +0100

    start

---
+++ SiteCV/SiteCV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiteCV.Startup))]
+namespace SiteCV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
